2801db41827b51128c0ac65e7bd1f3a5c1e2945b
saksung <you253804@gmail.com>
2017-12-12 09:42:03 +0700

    TestSwitchCase

--- src/oop_lab3/TestSwitchCase.java
+++ src/oop_lab3/TestSwitchCase.java
@@ -1,4 +1,28 @@
 package oop_lab3;
 
+import jdk.internal.dynalink.beans.StaticClass;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class TestSwitchCase {
-}
+    public Static void maid (String[]args) throws IOException {
+        //switch-case
+        BufferedReader reader=new BufferedReader(
+                new InputStreamReader(System.in));
+        int input;
+        System.out.print("Plese enter number (1-3): ");
+        input = Integer.parseInt(reader.readLine());
+        switch (input){
+            case 1: System.out.print("You entered 1. ");break;
+            case 2: System.out.print("You entered 2. ");break;
+            case 3: System.out.print("You entered 3. ");break;
+            default: System.out.print("Please  entered number 1-3. ");break;
+
+
+        }//switch
+
+    }//main
+}//Class
+
