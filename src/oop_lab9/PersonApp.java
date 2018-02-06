3c8fb73570df0005afc87e5d5b348ed83c539b24
saksung <you253804@gmail.com>
2018-02-06 11:03:53 +0700

    PersonAdd

--- src/oop_lab9/PersonApp.java
+++ src/oop_lab9/PersonApp.java
@@ -2,16 +2,19 @@ package oop_lab9;
 
 public class PersonApp {
     public  static void (String[]args){
-        Engine engine = new Engine("MOt","v-TEC");
-        //car
-        Person Person =new Person("Honda","Black",engine);
-        System.out.println(car.toString());
-
-        car car1=new car ("Honda","Red",engine);
-        System.out.println(car1.toString());
 
 
-        
+        Address Address = new Address("12/5","Suarat","80110");
+
+        Job Job = new Job("Police","1200");
+        System.out.println(Job.toString());
+
+        Person Person = new Person ("ID123454","Paut",Address,Job);
+        System.out.println(Person.toString());
+
+
+
+
     }//main
 }//class
 
