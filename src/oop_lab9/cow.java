2c4ada597aee4d17751e68841b0e195397228511
saksung <you253804@gmail.comg>
2018-02-13 09:20:54 +0700

    Pet

--- src/oop_lab9/cow.java
+++ src/oop_lab9/cow.java
@@ -2,9 +2,12 @@ package oop_lab9;
 
 import oop_lab8.Pet;
 
-public class cow extends Pet {
-    @overridde
-    public void makeNoise(){
-
-    }//main
-}//class
+public class Cow extends Pet {
+    public Cow(String name, int age) {
+        super(name, age);
+    }
+    @Override
+    public void makeNoise() {
+        System.out.println("More More !!!");
+    }
+}
